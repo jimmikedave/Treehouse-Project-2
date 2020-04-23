bd559c39ee6b83ef953d336f194bb1723cc455b0
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2020-04-23 14:02:15 -0400

    Added addEventListener and comments

--- js/script.js
+++ js/script.js
@@ -3,80 +3,64 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/***
-   Add your global variables that store the DOM elements you will
-   need to reference and/or manipulate.
-
-   But be mindful of which variables should be global and which
-   should be locally scoped to one of the two main functions you're
-   going to create. A good general rule of thumb is if the variable
-   will only be used inside of a function, then it can be locally
-   scoped to that function.
-***/
-
 const studentList = document.querySelectorAll('.student-item');
 const pageLimit = 10;
 
-
-
-
-/***
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
-
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function
-***/
+//this function filters each item according to the assigned page number
 const showPage = (list, page) => {
-  const startIndex = (page * pageLimit) - pageLimit;
-  const endIndex = (page * pageLimit);
-  for (i = 0; i < list.length; i +=1) {
-    if (startIndex <= list[i] && list[i] < endIndex){
-      studentList[i].style.display = '';
+  //startIndex is the first item on the page
+  const startIndex = parseInt((page * pageLimit) - pageLimit);
+  //endIndex is the last item on the page
+  const endIndex = parseInt((page * pageLimit));
+  //for loop creates assigns a number to each list item
+  for (i = 0; i < list.length; i += 1) {
+    //if else filters each item based on page number
+    if (i >= startIndex && i < endIndex) {
+      list[i].style.display = '';
     } else {
-      studentList[i].style.display = 'none';
+      list[i].style.display = 'none';
     }
   };
 }
-
-
-
-/***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
-***/
+/*this function creates pagination and loads a set of items
+when pagination buttons are clicked*/
 const appendPageLinks = (list) => {
+  //creates the total amount of pages needed as a variable
   const totalPages = studentList.length / pageLimit;
   const div = document.createElement('div');
-  const page = document.querySelector('page');
-    div.className = 'pagination';
-    page.appendChild(div);
+  const page = document.querySelector('.page');
   const ul = document.createElement('ul');
+    //gives div the class name pagination
+    div.className = 'pagination';
+    //assigns ul to div and div to div
     div.appendChild(ul);
-  for(i = 0; i < totalPages; i += 1)
+    page.appendChild(div);
+  //for loop creates each page for pagination
+  for(i = 0; i < totalPages; i += 1){
     const li = document.createElement('li');
     const a = document.createElement('a');
+    //brings the page to the top when clicked
+    a.href = '#';
+    //assigns the page it's number
     a.textContent = i + 1;
-    a.href = 'link';
     li.appendChild(a);
     ul.appendChild(li);
-
-    
-}
-
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+    //sets the first page as active
+    if (i == 0 ){
+      a.className = 'active';
+    }
+    /*clears all a element classes when clicked and
+    assigns the clicked a element's class as 'active'*/
+    a.addEventListener('click', (e) => {
+      const pagination = document.querySelectorAll('a');
+      for(i = 0; i < totalPages; i += 1){
+        pagination[i].className = '';
+      }
+      showPage(studentList, e.target.textContent);
+      e.target.className = 'active';
+        });
+      }
+    }
+//need to call the showPage function to show the first page
+showPage(studentList, 1);
+appendPageLinks(studentList);
